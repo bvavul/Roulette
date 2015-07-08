8ec57b091af8a31b7cd9d1df32081a987c074dcd
Thirty-Ninene <student10@js-c02mxpvdg083.punahou.edu>
2015-07-07 14:13:06 -1000

    stuff

--- appRoulette.js
+++ appRoulette.js
@@ -2,30 +2,26 @@ var computerChoice= "";
 var userStreak = "";
 function losingchance() {
 	computerChoice = Math.floor(Math.random()*6);
+		userStreak++;
 		if(computerChoice === 1) {
 			computerChoice = "chosen";
 				alert("You won! Your streak is " + userStreak + " wins.");
-				userStreak++;
 		}
 		if(computerChoice === 2) {
 			computerChoice = "chosen";
 				alert("You won! Your streak is " + userStreak + " wins.");
-				userStreak++;
 		}
 		if(computerChoice === 3) {
 			computerChoice = "chosen";
 				alert("You won! Your streak is " + userStreak + " wins.");
-				userStreak++;
 		}
 		if(computerChoice === 4) {
 			computerChoice = "chosen";
 				alert("You won! Your streak is " + userStreak + " wins.");
-				userStreak++;
 		}
 		if(computerChoice === 5) {
 			computerChoice = "chosen";
 				alert("You won! Your streak is " + userStreak + " wins.");
-				userStreak++;
 		}
 		if(computerChoice === 6) {
 			comptuerChoice = "lost";
