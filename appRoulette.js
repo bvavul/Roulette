0920f846fce4829b4beb5025d824b642bac71e5e
Thirty-Ninene <student10@js-c02mxpvdg083.punahou.edu>
2015-07-08 13:56:36 -1000

    ihowefhiorono

--- appRoulette.js
+++ appRoulette.js
@@ -3,32 +3,33 @@ var userStreak = "";
 function losingchance() {
 	userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
-		if(computerChoice === 1) {
-			computerChoice = "chosen";
-				displayUserScore(userStreak);
-		}
-		if(computerChoice === 2) {
-			computerChoice = "chosen";
-				displayUserScore(userStreak);
-		}
-		if(computerChoice === 3) {
-			computerChoice = "chosen";
-				displayUserScore(userStreak);
-		}
-		if(computerChoice === 4) {
-			computerChoice = "chosen";
-				displayUserScore(userStreak);
-		}
-		if(computerChoice === 5) {
-			computerChoice = "chosen";
-				displayUserScore(userStreak);
-		}
-		else {
+		if (computerChoice === 6){
 			computerChoice = "lost";
 				userStreak = 0;
 		}
+		else if(computerChoice === 1) {
+			computerChoice = "chosen";
+				displayUserScore(userStreak);
+		}
+		else if(computerChoice === 2) {
+			computerChoice = "chosen";
+				displayUserScore(userStreak);
+		}
+		else if(computerChoice === 3) {
+			computerChoice = "chosen";
+				displayUserScore(userStreak);
+		}
+		else if(computerChoice === 4) {
+			computerChoice = "chosen";
+				displayUserScore(userStreak);
+		}
+		else if(computerChoice === 5) {
+			computerChoice = "chosen";
+				displayUserScore(userStreak);
+		}
 }
 
- window.onbeforeunload = function() {
-        return "Press the 'stay on page' button below. I put a brick wall so this won't refresh(Due to glitches).";
+window.onbeforeunload = function() {
+    return "Press the 'stay on page' button below. I put a brick wall so this won't refresh(Due to glitches).";
 };
+
