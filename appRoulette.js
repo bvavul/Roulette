a4af9d3300e1bcc3d4635940f89517b1511fb4dc
kpick6 <student10@js-c02mxpvdg083.punahou.edu>
2015-07-13 13:54:51 -1000

    3rfds

--- appRoulette.js
+++ appRoulette.js
@@ -1,34 +1,27 @@
-
 var computerChoice = "";
 var userStreak = "";
 function WinnerOrNotHereICome() {
 	userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
 		if (computerChoice === 5){
-			computerChoice = "lost";
 				displayUserScore(userStreak);
 				alert("Gooood Morning(Or afternoon. Or night.) You died a horrible, slow, painful death. Try not to visualize this. Oops, you might have already. But hey, wanna hear a secret? Get to 100 streak and I'll tell you. Please try again. Sadly, your streak was " + userStreak + " wins.");
 				userStreak = 0;
 		}
 		else if(computerChoice === 1) {
-			computerChoice = "chosen";
 				displayUserScore(userStreak);
 		}
 		else if(computerChoice === 2) {
-			computerChoice = "chosen";
 				displayUserScore(userStreak);
 		}
 		else if(computerChoice === 3) {
-			computerChoice = "chosen";
 				displayUserScore(userStreak);
 		}
 		else if(computerChoice === 4) {
-			computerChoice = "chosen";
 				displayUserScore(userStreak);
 		}
 		else
 			computerChoice = "lost";
-				computerChoice = "lost";
 				userScore = 0;
 return;
 }
