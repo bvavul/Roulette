f5fdbd81b54bc9b44b1ab1a0542202d21e137893
kpick6 <student10@js-c02mxpvdg083.punahou.edu>
2015-07-13 12:48:39 -1000

    ewrswsf

--- appRoulette.js
+++ appRoulette.js
@@ -1,11 +1,12 @@
-var computerChoice= "";
+var computerChoice = "";
 var userStreak = "";
 var highStreak = "20";
-function lifeordeath() {
+function WinnerOrNot() {
 	userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
 		if (computerChoice === 5){
 			computerChoice = "chosen";
+				displayuserScore(userStreak);
 		}
 		else if(computerChoice === 1) {
 			computerChoice = "chosen";
@@ -23,18 +24,35 @@ function lifeordeath() {
 			computerChoice = "chosen";
 				displayUserScore(userStreak);
 		}
+		else if(userStreak > 99) {
+			userStreak = 0;
+				alert("The secret is that you just realized that you have been playing this for too long. And you just defied mathematical probability(Which is 0.00166666666 in one.). And I reset your streak to zero. TROLLLLLEEEDDDD");
+
+		}
+		else if(userStreak > highStreak) {
+			document.getElementById('something').innerHTML = "Woah! You broke the highscore! The new highscore is " + userStreak + ".";
+				highStreak = userStreak;
+		}
 		else {
 			computerChoice = "lost";
 				displayUserScore(userStreak);
-				alert("Gooood Morning(Or afternoon. Or night.) You died a horrible, slow, painful death. Try not to visualize this. Oops, you might have already. Wanna hear a secret? Get to 100 streak and I'll tell you. Please try again. Your streak was " + userStreak + " wins.");
-				
-				/*
+				alert("Gooood Morning(Or afternoon. Or night.) You died a horrible, slow, painful death. Try not to visualize this. Oops, you might have already. But hey, wanna hear a secret? Get to 100 streak and I'll tell you. Please try again. Sadly, your streak was " + userStreak + " wins.");
+				userStreak = 0;
+
+		}
+	return;
+}
+var displayUserScore = function() {
+	document.getElementById('score').innerHTML = "Your streak is " + userStreak + ".";
+};
+
+/*
 LES DO DIS
  _________
-/_______  \_______
-	    | |	     |
-	    | |	     |
-	    | |______|_
+/_______  \______
+	    | | _  _ |
+	    | |	 __  |
+	    | |______|___
 	    |_|        | |
 	    |_|	       | |
 	    | |	       | |
@@ -42,24 +60,5 @@ LES DO DIS
 	    | |	       |  
 	    | |		    | 
 	    |_|__________|
-
 YOU LOST HAHAHAHAHAHAH YOU LOSERLOSERLOSERLOSERLOSER
 */
-			userStreak = 0;
-		}
-		if (userStreak > highStreak) {
-			alert("Woah! You broke the highscore! The new highscore is " + userStreak);
-				highStreak = userStreak;
-		}
-
-
-		if (userStreak > 99.1010001000101000100100101000101000101010001000101000101001010001010000100010100010000001010010100010100010010101000100010001000101010001010010100100100101000000101010010001010101000101010100001001000010010010100100010001010001010001010001001000001010100000101010001010101010101010010101010100010101010010100010101010101010101010101010100010101010011010010101000010101001010000101010001000100101000010100100100010101010101010010010100010101010100010101010101010001000100100010001010000100010001000000010100001001001010101011001000100101001001000100100101001010100100010101010001000010100100100101010101010001000010100100101001010101010100100010001001010000100100100000100000100100101000101001010010011010010101010010010010010010001010010010000101000010100000100001001000100000001010101010101010101010101010101010100010010010000000010100100101010001010000100101000001000010010000101001) {
-			alert("The secret is that you just realized that you have been playing this for too long. And you just defied mathematical probability(Which is 0.00166666666 in one.)");
-
-return;
-		}
-}
-var displayUserScore = function() {
-	document.getElementById('score').innerHTML = "Your streak is " + userStreak + ".";
-	return;
-};
