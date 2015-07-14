039d85e9737a3d89104b5a6183a3f892807d9878
kpick6 <student10@js-c02mxpvdg083.punahou.edu>
2015-07-14 13:01:53 -1000

    Why it no work

--- appRoulette.js
+++ appRoulette.js
@@ -4,27 +4,27 @@ function WinnerOrNot() {
 	userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
 		if (computerChoice === 1) {
-				displayUserScore(userStreak);
+			displayUserScore(userStreak);
 		}
 		else if(computerChoice === 2) {
-				displayUserScore(userStreak);
+			displayUserScore(userStreak);
 		}
 		else if(computerChoice === 3) {
-				displayUserScore(userStreak);
+			displayUserScore(userStreak);
 		}
 		else if(computerChoice === 4) {
-				displayUserScore(userStreak);
+			displayUserScore(userStreak);
 		}
 		else if(computerChoice === 5) {
-				displayUserScore(userStreak);
+			displayUserScore(userStreak);
 		}
 		else
-				alert("Gooood Morning(Or afternoon. Or night.) You died a horrible, slow, painful death. Try not to visualize this. Oops, you might have already. But hey, wanna hear a secret? Get to 100 streak and I'll tell you. Please try again. Sadly, your streak was " + userStreak + " wins.");
-				userScore = 0;
-return;
+			alert("Gooood Morning(Or afternoon. Or night.) You died a horrible, slow, painful death. Try not to visualize this. Oops, you might have already. But hey, wanna hear a secret? Get to 100 streak and I'll tell you. Please try again. Sadly, your streak was " + userStreak + " wins.");
+			userScore = 0;
 }
 var displayUserScore = function() {
 	document.getElementById('paragraph').innerHTML = "Your streak is " + userStreak + ".";
+	return;
 };
 /*
 LES DO DIS
