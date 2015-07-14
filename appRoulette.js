e11e9a954d8ba1b76e015f880e487f9a29c29c51
kpick6 <student10@js-c02mxpvdg083.punahou.edu>
2015-07-14 13:59:56 -1000

    bruh

--- appRoulette.js
+++ appRoulette.js
@@ -1,6 +1,7 @@
 var computerChoice;
 var userStreak = 0;
 function WinnerOrNot() {
+userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
 		if (computerChoice === 1) {
 			displayUserScore(userStreak);
