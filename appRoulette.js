1ab6e0a3c7ded292bb139d70361a5c0a3be0705e
kpick6 <student10@js-c02mxpvdg083.punahou.edu>
2015-07-14 13:19:14 -1000

    yay

--- appRoulette.js
+++ appRoulette.js
@@ -5,18 +5,23 @@ function WinnerOrNot() {
 	computerChoice = Math.floor(Math.random()*6);
 		if (computerChoice === 1) {
 			displayUserScore(userStreak);
+			onehundredcheck(userStreak);
 		}
 		else if(computerChoice === 2) {
 			displayUserScore(userStreak);
+			onehundredcheck(userStreak);
 		}
 		else if(computerChoice === 3) {
 			displayUserScore(userStreak);
+			onehundredcheck(userStreak);
 		}
 		else if(computerChoice === 4) {
 			displayUserScore(userStreak);
+			onehundredcheck(userStreak);
 		}
 		else if(computerChoice === 5) {
 			displayUserScore(userStreak);
+			onehundredcheck(userStreak);
 		}
 		else
 			alert("Gooood Morning(Or afternoon. Or night.) You died a horrible, slow, painful death. Try not to visualize this. Oops, you might have already. But hey, wanna hear a secret? Get to 100 streak and I'll tell you. Please try again. Sadly, your streak was " + userStreak + " wins.");
@@ -27,6 +32,7 @@ var displayUserScore = function() {
 };
 function onehundredcheck() {
 	if (userStreak > 99) {
+		userStreak = 0;
 		alert("You have broken the 100 streak. The secret is that you have been playing this for too long, and you have no friends. JUST STOP PLAYING AND GO OUTSIDE OR SOMETHING. And I reset your score to 0. TTTRRROOOOOLLLLEEEDDD");
 	}
 }
