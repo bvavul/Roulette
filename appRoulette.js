cc0784be8d08278d2733f7af3f5ba899e207805c
kpick6 <student10@js-c02mxpvdg083.punahou.edu>
2015-07-14 13:58:27 -1000

    ah

--- appRoulette.js
+++ appRoulette.js
@@ -1,32 +1,37 @@
 var computerChoice;
 var userStreak = 0;
 function WinnerOrNot() {
-	userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
 		if (computerChoice === 1) {
+			userStreak++;
 			displayUserScore(userStreak);
 			onehundredcheck(userStreak);
 		}
 		else if(computerChoice === 2) {
+			userStreak++;
 			displayUserScore(userStreak);
 			onehundredcheck(userStreak);
 		}
 		else if(computerChoice === 3) {
+			userStreak++;
 			displayUserScore(userStreak);
 			onehundredcheck(userStreak);
 		}
 		else if(computerChoice === 4) {
+			userStreak++;
 			displayUserScore(userStreak);
 			onehundredcheck(userStreak);
 		}
 		else if(computerChoice === 5) {
+			userStreak++;
 			displayUserScore(userStreak);
 			onehundredcheck(userStreak);
 		}
 		else
 			alert("Gooood Morning(Or afternoon. Or night.) You died a horrible, slow, painful death. Try not to visualize this. Oops, you might have already. But hey, wanna hear a secret? Get to 100 streak and I'll tell you. Please try again. Sadly, your streak was " + userStreak + " wins.");
 			userStreak = 0;
-return;
+		return;
+
 }
 var displayUserScore = function() {
 	document.getElementById('paragraph').innerHTML = "Your streak is " + userStreak + ".";
