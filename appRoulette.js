c5ab168ffd88f836618ce88c290959adf45e0d2f
Hitlerese <student10@js-c02mxpvdg083.punahou.edu>
2015-07-15 13:36:42 -1000

    gresfdc

--- appRoulette.js
+++ appRoulette.js
@@ -1,26 +1,22 @@
 var computerChoice = 20;
 var userStreak = "";
 function WinnerOrNot() {
+	userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
 		if (computerChoice === 1) {
 			displayUserScore(userStreak);
-			userStreak++;
 		}
 		else if(computerChoice === 2) {
 			displayUserScore(userStreak);
-			userStreak++;
 		}
 		else if(computerChoice === 3) {
 			displayUserScore(userStreak);
-			userStreak++;
 		}
 		else if(computerChoice === 4) {
 			displayUserScore(userStreak);
-			userStreak++;
 		}
 		else if(computerChoice === 5) {
 			displayUserScore(userStreak);
-			userStreak++;
 		}
 		else {
 			userStreak--;
