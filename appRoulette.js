f8f7fb87f6e41a022d7787cc704c620b3baf1017
Hitlerese <student10@js-c02mxpvdg083.punahou.edu>
2015-07-15 13:30:23 -1000

    IT WORKSSSSS

--- appRoulette.js
+++ appRoulette.js
@@ -1,27 +1,26 @@
 var computerChoice = 20;
 var userStreak = "";
 function WinnerOrNot() {
-	userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
 		if (computerChoice === 1) {
 			displayUserScore(userStreak);
-			onehundredcheck(userStreak);
+			userStreak++;
 		}
 		else if(computerChoice === 2) {
 			displayUserScore(userStreak);
-			onehundredcheck(userStreak);
+			userStreak++;
 		}
 		else if(computerChoice === 3) {
 			displayUserScore(userStreak);
-			onehundredcheck(userStreak);
+			userStreak++;
 		}
 		else if(computerChoice === 4) {
 			displayUserScore(userStreak);
-			onehundredcheck(userStreak);
+			userStreak++;
 		}
 		else if(computerChoice === 5) {
 			displayUserScore(userStreak);
-			onehundredcheck(userStreak);
+			userStreak++;
 		}
 		else {
 			alert("Gooood Morning(Or afternoon. Or night.) You died a horrible, slow, painful death. Please try again. Sadly, your streak was " + userStreak + " wins.");
