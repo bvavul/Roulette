651a3304f26a871048782282d0a78f5b4095370d
The Thirty-Ninene <liobahawaii@icloud.com>
2015-07-15 17:23:05 -1000

    Update appRoulette.js
    IT BROKE

--- appRoulette.js
+++ appRoulette.js
@@ -1,34 +1,31 @@
 var highStreak = 20;
 var computerChoice = 0;
 var userStreak = "";
+var bananas = 0.01;
+var storagevalue = 99;
 function WinnerOrNot() {
 	userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
-		if(computerChoice === 1) {
+		if (computerChoice === 1) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			scanForHighStreak(userStreak, highStreak);
 		}
 		else if(computerChoice === 2) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			scanForHighStreak(userStreak, highStreak);
-
 		}
 		else if(computerChoice === 3) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			scanForHighStreak(userStreak, highStreak);
 		}
 		else if(computerChoice === 4) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			scanForHighStreak(userStreak, highStreak);
+
 		}
 		else if(computerChoice === 5) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			scanForHighStreak(userStreak, highStreak);
 		}
 		else {
 			userStreak--;
@@ -36,14 +33,25 @@ function WinnerOrNot() {
 			userStreak++;
 			userStreak = 0;
 		}
+
 	return;
 }
 var displayUserScore = function() {
 	document.getElementById('paragraph').innerHTML = "Your streak is " + userStreak + ".";
 };
+
 var displayHighStreak = function() {
 	document.getElementById('paragraph2').innerHTML = "The highscore is " + highStreak + ".";
 };
+
+var retrieveMemories = function() {
+	if (storagevalue > 100) {
+    // Store
+	localStorage.setItem("memory", highStreak);
+	// Retrieve
+	document.getElementById("paragraph3").innerHTML = localStorage.getItem("memory");
+};
+
 /*
 LES DO DIS
  _________
@@ -58,5 +66,5 @@ LES DO DIS
 	    | |	       |
 	    | |		    | 
 	    |_|__________|
-YOU LOST HAHAHAHAHAHAH YOU LOSERLOSERLOSERLOSERLOSER
+YOU LOST HAHAHAHAHAHAHA YOU LOSERLOSERLOSERLOSERLOSER
 */
