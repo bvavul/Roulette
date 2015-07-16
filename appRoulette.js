e3a7bb07fb30fa9393f3aee54a30c463571633da
Hitlerese <student10@js-c02mxpvdg083.punahou.edu>
2015-07-15 14:22:16 -1000

    asedxf4erds

--- appRoulette.js
+++ appRoulette.js
@@ -43,7 +43,7 @@ var displayUserScore = function() {
 	document.getElementById('paragraph').innerHTML = "Your streak is " + userStreak + ".";
 };
 var displayHighStreak = function() {
-	document.getElementById('paragraph2').innerHTML = "Try to beat " + highStreak + ".";
+	document.getElementById('paragraph2').innerHTML = "The highscore is " + highStreak + ".";
 };
 
 function scanForHighStreak() {
