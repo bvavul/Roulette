e4df0158e3118d764c6899c676d9e7b2efb68841
Hitlerese <student10@js-c02mxpvdg083.punahou.edu>
2015-07-15 14:15:20 -1000

    YAY

--- appRoulette.js
+++ appRoulette.js
@@ -1,29 +1,35 @@
 var highStreak = 20;
 var computerChoice = 20;
 var userStreak = "";
+var beathighhowmany = 0;
 function WinnerOrNot() {
 	userStreak++;
 	computerChoice = Math.floor(Math.random()*6);
-		if (computerChoice === 1) {
+		if(computerChoice === 1) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
+			scanForHighStreak(userStreak, highStreak);
 		}
 		else if(computerChoice === 2) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
+			scanForHighStreak(userStreak, highStreak);
 
 		}
 		else if(computerChoice === 3) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
+			scanForHighStreak(userStreak, highStreak);
 		}
 		else if(computerChoice === 4) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
+			scanForHighStreak(userStreak, highStreak);
 		}
 		else if(computerChoice === 5) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
+			scanForHighStreak(userStreak, highStreak);
 		}
 		else {
 			userStreak--;
@@ -39,6 +45,11 @@ var displayUserScore = function() {
 var displayHighStreak = function() {
 	document.getElementById('paragraph2').innerHTML = "The highscore is " + highStreak + ".";
 };
+
+function scanForHighStreak() {
+	if(userStreak > highStreak)
+		highStreak = userStreak;
+}
 
 /*
 LES DO DIS
