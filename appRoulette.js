17213354ddb9fd57ab4e4e3c534f9033699415f5
The Thirty-Ninene <student10@js-c02mxpvdg083.punahou.edu>
2015-07-17 13:00:34 -1000

    asdzfsfdz

--- appRoulette.js
+++ appRoulette.js
@@ -7,27 +7,27 @@ function WinnerOrNot() {
 		if(computerChoice === 1) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			checkForHighScore(highStreak);
+			checkForHighScore();
 		}
 		else if(computerChoice === 2) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			checkForhighScore(highStreak);
+			checkForhighScore();
 		}
 		else if(computerChoice === 3) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			checkForhighScore(highStreak);
+			checkForhighScore();
 		}
 		else if(computerChoice === 4) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			checkForhighScore(highStreak);
+			checkForhighScore();
 		}
 		else if(computerChoice === 5) {
 			displayUserScore(userStreak);
 			displayHighStreak(highStreak);
-			checkForhighScore(highStreak);
+			checkForhighScore();
 		}
 		else {
 			userStreak--;
@@ -45,11 +45,23 @@ var displayHighStreak = function() {
 };
 
 function checkForHighScore() {
-	if (userStreak > highStreak) {
+	if(userStreak > highStreak) {
 		highStreak = userStreak;
+	}
+	else {
+		document.getElementById('paragraph3').innerHTML = "The highscore is " + highStreak + ".";
 	}
 }
 
+function onehundred() {
+	if(userStreak > 100) {
+		userStreak = 0;
+		alert("You have reached 100 streak. Good job. You just defied mathematical probability(Which is 1/6000. SIX THOUSAND.). And I reset your streak to 0. TROOOLLLLLEEEEDDD");
+	}
+	else {
+		checkForHighScore();
+	}
+}
 
 /*
 LES DO DIS
